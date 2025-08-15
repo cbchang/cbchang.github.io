18f2f4e38fda75bd2c2b745aee5d2b2812ff4f28
cbchang <34696402+cbchang@users.noreply.github.com>
2025-08-15 19:59:33 +0000

    Deploying to gh-pages from @ cbchang/cbchang.github.io@c6593e7385f8ab6fe30f8df46d5b8f6b8439d4fd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,7 +63,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-published-with-esther-de-leeuw-phonetic-and-phonological-l1-attrition-and-drift-in-bilingual-speech-de-leeuw-amp-amp-chang-2024-in-the-cambridge-handbook-of-bilingual-phonetics-and-phonology-thanks-to-mark-amengual-for-his-expert-editorship",
+            },},{id: "news-published-with-esther-de-leeuw-phonetic-and-phonological-l1-attrition-and-drift-in-bilingual-speech-in-the-cambridge-handbook-of-bilingual-phonetics-and-phonology-thanks-to-mark-amengual-for-his-expert-editorship",
           title: 'Published (with Esther de Leeuw) “Phonetic and phonological L1 attrition and drift in...',
           description: "",
           section: "News",},{id: "news-gave-three-talks-and-one-workshop-in-the-fall-at-lmu-munich-germany-in-october-at-aarhus-university-denmark-in-november-and-at-simon-fraser-university-canada-in-december-️-️-️",
