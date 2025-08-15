2dff51c3e9e59529a7898b28617fa5c3aaa4f16e
cbchang <34696402+cbchang@users.noreply.github.com>
2025-08-15 20:34:54 +0000

    Deploying to gh-pages from @ cbchang/cbchang.github.io@26b558e1a2776ae0cde2d8fb42c617ea118d625c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,7 +63,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-published-with-esther-de-leeuw-phonetic-and-phonological-l1-attrition-and-drift-in-bilingual-speech-in-the-cambridge-handbook-of-bilingual-phonetics-and-phonology-thanks-to-mark-amengual-for-his-expert-editorship",
+            },},{id: "news-started-a-new-position-as-professor-in-the-department-of-linguistics-and-translation-at-city-university-of-hong-kong",
+          title: 'Started a new position as Professor in the Department of Linguistics and Translation...',
+          description: "",
+          section: "News",},{id: "news-published-with-esther-de-leeuw-phonetic-and-phonological-l1-attrition-and-drift-in-bilingual-speech-in-the-cambridge-handbook-of-bilingual-phonetics-and-phonology-thanks-to-mark-amengual-for-his-expert-editorship",
           title: 'Published (with Esther de Leeuw) “Phonetic and phonological L1 attrition and drift in...',
           description: "",
           section: "News",},{id: "news-gave-three-talks-and-one-workshop-in-the-fall-at-lmu-munich-germany-in-october-at-aarhus-university-denmark-in-november-and-at-simon-fraser-university-canada-in-december-️-️-️",
